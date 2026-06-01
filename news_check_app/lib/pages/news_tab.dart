bdce76e914625c2de5c5bf704d276a653555eebd
GraftCopolymer <2819952726@qq.com>
2026-06-01 23:55:54 +0800

    feat(ui): NewsTab 改用 MasonryGridView 瀑布流布局

--- news_check_app/lib/pages/news_tab.dart
+++ news_check_app/lib/pages/news_tab.dart
@@ -1,15 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/instance_manager.dart';
 import 'package:news_check_app/controllers/news_list_controller.dart';
 import 'package:news_check_app/pages/news_detail_page.dart';
-import 'package:news_check_app/utils/store_keys.dart';
-import 'package:news_check_app/utils/store_utils.dart';
-import 'package:news_check_app/utils/token_utils.dart';
-import 'package:news_check_app/widgets/news_simple_card.dart';
+import 'package:news_check_app/widgets/shimmer_loading.dart';
+import 'package:news_check_app/widgets/staggered_news_card.dart';
 
 class NewsTab extends StatefulWidget {
   const NewsTab({super.key});
@@ -21,19 +20,12 @@ class NewsTab extends StatefulWidget {
 class _NewsTabState extends State<NewsTab> with AutomaticKeepAliveClientMixin {
   final _newsListController = Get.put(NewsListController());
 
-  Future<void> _test() async {
-    final token = await StoreUtils.secure.read(key: StoreKeys.TOKEN); 
-    debugPrint("用户 Token: $token");
-    final model = TokenUtils.parseTokenModel(token!);
-    debugPrint("$model");
-  }
-
   @override
   void initState() {
     super.initState();
     _newsListController.loadMore().onError((e, _) {
       if (e is DioException) {
-        if (e.response?.statusCode == 401)  {
+        if (e.response?.statusCode == 401) {
           Fluttertoast.showToast(msg: "登录过期, 请重新登录");
         } else {
           Fluttertoast.showToast(msg: "未知错误");
@@ -42,44 +34,59 @@ class _NewsTabState extends State<NewsTab> with AutomaticKeepAliveClientMixin {
         Fluttertoast.showToast(msg: "未知错误");
       }
     });
-    _test();
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("IT 新闻"),
-      ),
-      body: NotificationListener<ScrollNotification>(
-        onNotification: (scrollNotification) {
-          if (scrollNotification.metrics.maxScrollExtent == scrollNotification.metrics.pixels) {
-            _newsListController.loadMore();
-            return true;
-          }
-          return false;
-        },
-        child: Obx(() {
-          return RefreshIndicator(
-            onRefresh: () async {
-              _newsListController.refreshList();
+    return NotificationListener<ScrollNotification>(
+      onNotification: (scrollNotification) {
+        if (scrollNotification.metrics.maxScrollExtent ==
+            scrollNotification.metrics.pixels) {
+          _newsListController.loadMore();
+          return true;
+        }
+        return false;
+      },
+      child: Obx(() {
+        if (_newsListController.newsList.isEmpty) {
+          return const ShimmerGrid();
+        }
+        return RefreshIndicator(
+          onRefresh: () async {
+            _newsListController.refreshList();
+          },
+          child: MasonryGridView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+            crossAxisSpacing: 12,
+            mainAxisSpacing: 12,
+            itemCount: _newsListController.newsList.length,
+            gridDelegate:
+                const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+            ),
+            itemBuilder: (context, index) {
+              final news = _newsListController.newsList[index];
+              // 根据标题长度决定高度
+              final height = news.title.length <= 25
+                  ? 200.0
+                  : news.title.length <= 40
+                      ? 250.0
+                      : 280.0;
+              return StaggeredNewsCard(
+                news: news,
+                height: height,
+                onTap: (n) {
+                  Get.to(() => NewsDetailPage(newsId: n.id));
+                },
+              );
             },
-            child: ListView.builder(
-              itemCount: _newsListController.newsList.length,
-              itemBuilder: (context, index) {
-                final newsSimple = _newsListController.newsList[index];
-                return NewsSimpleCard(news: newsSimple, onTap: (news) {
-                    // 跳转到新闻详情页
-                    Get.to(() => NewsDetailPage(newsId: newsSimple.id,));
-                },);
-            }),
-          );
-        })
-      ),
+          ),
+        );
+      }),
     );
   }
-  
+
   @override
   bool get wantKeepAlive => true;
 }
