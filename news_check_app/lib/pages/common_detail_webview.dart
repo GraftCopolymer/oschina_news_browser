a3af05f97f176e09077c0208daa100f3f7929ff0
GraftCopolymer <2819952726@qq.com>
2026-06-02 14:44:35 +0800

    refactor: CommonDetailWebView with settings injection, progress channel, TOC scroll

--- news_check_app/lib/pages/common_detail_webview.dart
+++ news_check_app/lib/pages/common_detail_webview.dart
@@ -2,21 +2,31 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:news_check_app/controllers/reading_settings_controller.dart';
 import 'package:news_check_app/utils/passage_utils.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class CommonDetailWebView extends StatefulWidget {
   const CommonDetailWebView({
-    super.key, 
-    required this.htmlContent, 
+    super.key,
+    required this.htmlContent,
     required this.webViewKey,
-    this.onImageClick
+    this.onImageClick,
+    this.onProgressChanged,
+    this.onScrollChanged,
+    required this.tocEntries,
+    this.scrollToTocNotifier,
   });
 
   final String htmlContent;
   final GlobalKey webViewKey;
   final Function(Map<String, dynamic> data)? onImageClick;
+  final Function(int progress)? onProgressChanged;
+  final Function(int scrollTop)? onScrollChanged;
+  final List<TocEntry> tocEntries;
+  final ValueNotifier<String?>? scrollToTocNotifier;
 
   @override
   State<CommonDetailWebView> createState() => _CommonDetailWebViewState();
@@ -24,28 +34,64 @@ class CommonDetailWebView extends StatefulWidget {
 
 class _CommonDetailWebViewState extends State<CommonDetailWebView> {
   late final WebViewController _controller;
+  final _settingsCtrl = Get.find<ReadingSettingsController>();
+
+  void injectSettings() {
+    _controller.runJavaScript(_settingsCtrl.injectCssVariablesJs);
+  }
+
+  String get _injectAllJs => '''
+    (function() {
+      ${_settingsCtrl.injectCssVariablesJs.replaceAll('(function() {', '').replaceAll('})();', '')}
+      var imgs = document.getElementsByTagName('img');
+      var ratio = window.devicePixelRatio || 1;
+      for (var i = 0; i < imgs.length; i++) {
+        imgs[i].onclick = function() {
+          var rect = this.getBoundingClientRect();
+          ImageHeroApp.postMessage(JSON.stringify({
+            src: this.src,
+            x: rect.left * ratio, y: rect.top * ratio,
+            width: rect.width * ratio, height: rect.height * ratio
+          }));
+        };
+      }
+      var ticking = false;
+      window.onscroll = function() {
+        if (!ticking) {
+          window.requestAnimationFrame(function() {
+            var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+            var scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            if (scrollHeight > 0) {
+              var progress = Math.round(scrollTop / scrollHeight * 100);
+              ReadingProgress.postMessage(JSON.stringify({progress: progress, scrollTop: scrollTop}));
+            }
+            ticking = false;
+          });
+          ticking = true;
+        }
+      };
+    })();
+  ''';
 
   @override
   void initState() {
     super.initState();
+    widget.scrollToTocNotifier?.addListener(_onScrollToTocCommand);
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setNavigationDelegate(
         NavigationDelegate(
           onNavigationRequest: (request) async {
-            // 后续在 WebView 内跳转都打开用户浏览器
-            final Uri uri = Uri.parse(request.url);
+            final uri = Uri.parse(request.url);
             if (await canLaunchUrl(uri)) {
               await launchUrl(uri, mode: LaunchMode.externalApplication);
             } else {
-              await Fluttertoast.cancel();
               await Fluttertoast.showToast(msg: "未检测到手机浏览器");
             }
             return NavigationDecision.prevent;
           },
-          onPageFinished: (url) {
-            // 注入图片点击事件
-            _controller.runJavaScript(PassageUtils.imageClickJs);
+          onPageFinished: (_) {
+            _controller.runJavaScript(_injectAllJs);
           },
         ),
       )
@@ -56,7 +102,32 @@ class _CommonDetailWebViewState extends State<CommonDetailWebView> {
           widget.onImageClick?.call(data);
         },
       )
+      ..addJavaScriptChannel(
+        "ReadingProgress",
+        onMessageReceived: (msg) {
+          final data = jsonDecode(msg.message) as Map<String, dynamic>;
+          final progress = data['progress'] as int;
+          final scrollTop = data['scrollTop'] as int;
+          widget.onProgressChanged?.call(progress);
+          widget.onScrollChanged?.call(scrollTop);
+        },
+      )
       ..loadHtmlString(widget.htmlContent);
+  }
+
+  void _onScrollToTocCommand() {
+    final tocId = widget.scrollToTocNotifier?.value;
+    if (tocId != null) {
+      _controller.runJavaScript('''
+        document.getElementById('$tocId').scrollIntoView({behavior: 'smooth', block: 'start'});
+      ''');
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.scrollToTocNotifier?.removeListener(_onScrollToTocCommand);
+    super.dispose();
   }
 
   @override
